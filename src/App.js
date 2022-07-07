d979b6830a740216e312ce4b6ab43e6fd29b1c98
raduxc44 <raduxc44@gmail.com>
2022-07-07 03:03:24 +0300

    Cards are now generated from the data.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Nav from './nav';
 import Card from './card';
-
+import data from './data';
 
 function App() {
+  let cards = data.map(card => {
+    return(
+      <Card 
+        key={card.id}
+        {...card}
+      />
+    )
+  })
   return (
     <div className="App">
       <Nav />
       <div className='card-container'>
-        <Card />
+        {cards}
       </div>
     </div>
   );
--- src/card.jsx
+++ src/card.jsx
@@ -1,25 +1,21 @@
-function Card() {
+function Card(props) {
     return(
         <div className="card">
-            <img src="https://source.unsplash.com/WLxQvbMyfas" alt="placeholder" />
+            <img src={props.imageUrl} alt="placeholder" />
             <div className="card-info">
                 <div className="card-upper">
                     <div className="horizontal">
                         <div className="title">
                             <i className="fa-solid fa-location-dot"></i>
-                            <p>JAPAN</p>
+                            <p>{props.title}</p>
                         </div>
                         <a href="https://www.google.com/maps/dir//35.360557,138.72767/@35.3628825,138.6573517,12zd" target={'_blank'} rel= 'noreferrer'>View on Google Maps</a>
                     </div>
-                    <h2>Mount Fuji</h2>
+                    <h2>{props.title}</h2>
                 </div>
                 <div className="card-lower">
-                    <h6>12 Jan, 2021 - 24 Jan 2021</h6>
-                    <p>Mount Fuji is the tallest mountain in Japan, 
-                        standing at 3,776 meters (12,380 feet). 
-                        Mount Fuji is the single most popular tourist site in Japan, 
-                        for both Japanese and foreign tourists.
-                    </p>
+                    <h6>{props.startDate} - {props.endDate}</h6>
+                    <p>{props.description}</p>
                 </div>
             </div>
         </div>
